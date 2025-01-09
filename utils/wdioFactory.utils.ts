ecce85fc7766d95ab7d6f3c1c9b0d75c4b8ee9fd
HarveyC <harveydavid14@gmail.com>
2025-01-09 16:37:41 -0500

    Add getSelectorByValue method to WdioFactoryUtils for dynamic element selection

--- utils/wdioFactory.utils.ts
+++ utils/wdioFactory.utils.ts
@@ -10,12 +10,30 @@ export default class WdioFactoryUtils {
 
     /**
     * Clicks on an element
-    * @param ObjElement an object containing the selector and description of the element
+    * @param objElement an object containing the selector and description of the element
     */
-    public async click(ObjElement: ObjElement): Promise<void> {
-        const elementSelector = $(ObjElement.selector);
-        const elementDescription = ObjElement.description;
+    public async click(objElement: ObjElement): Promise<void> {
+        const elementSelector = $(objElement.selector);
+        const elementDescription = objElement.description;
         await elementSelector.waitForClickable({timeoutMsg: `Element ${elementDescription} is not clickable before timeout`});
         await elementSelector.click();
     }
+
+
+    /**
+     * Returns the selector and description of an element by replacing the placeholder ${value} with the value provided
+     * @param objElement an object containing the selector and description of the element
+     * @param strValue the value to replace the placeholder ${value}
+     * @returns an object containing the selector and description of the element of type ObjElement
+     */
+    public async getSelectorByValue(objElement:ObjElement , strValue: string): Promise<ObjElement> {
+        const elementSelector = await objElement.selector.replace('${value}', strValue);
+        const elementDescription = await objElement.description.replace('${value}', strValue);
+        return {
+            selector: elementSelector,
+            description: elementDescription
+        };
+    }
+
+
 }
