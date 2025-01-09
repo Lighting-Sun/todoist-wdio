b29b31348662e20f44e33b28e6b3c253c6f901bc
HarveyC <harveydavid14@gmail.com>
2025-01-09 16:29:37 -0500

    Refactor click method in WdioFactoryUtils to use a dedicated interface for element parameters

--- utils/wdioFactory.utils.ts
+++ utils/wdioFactory.utils.ts
@@ -1,11 +1,18 @@
 import { $ } from '@wdio/globals'
 
 
+interface ObjElement {
+    selector: string;
+    description: string;
+}
 export default class WdioFactoryUtils {
 
 
-
-    public async click(ObjElement: {selector: string, description: string}): Promise<void> {
+    /**
+    * Clicks on an element
+    * @param ObjElement an object containing the selector and description of the element
+    */
+    public async click(ObjElement: ObjElement): Promise<void> {
         const elementSelector = $(ObjElement.selector);
         const elementDescription = ObjElement.description;
         await elementSelector.waitForClickable({timeoutMsg: `Element ${elementDescription} is not clickable before timeout`});
