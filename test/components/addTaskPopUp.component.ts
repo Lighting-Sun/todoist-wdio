4024dd4ab0b0b2be6ba665c9ee93b787a1c7958f
HarveyC <harveydavid14@gmail.com>
2025-01-16 12:15:16 -0500

    Add AddTaskPopUp component with methods for task name and description input

---
+++ test/components/addTaskPopUp.component.ts
@@ -0,0 +1,48 @@
+import BaseComponent from "./base.component";
+
+class AddTaskPopUp extends BaseComponent{
+
+    locators = {
+        taskNameInput:{
+            selector: "div.task_editor__content_field p",
+            description: "Task name input"
+        },
+        taskDescriptionInput:{
+            selector: "div.task_editor__description_field p",
+            description: "Task description input"
+        },
+        addTaskButton:{
+            selector: "button[data-testid='task-editor-submit-button']",
+            description: "Add task button"
+        }
+    }
+
+    /**
+     * Fills the task name input
+     * @param taskName the name of the task
+     * @returns Promise<void>
+     */
+    async fillTaskName (taskName: string): Promise<void> {
+        await this.wdioFactoryUtils.setValue(this.locators.taskNameInput, taskName);
+    }
+
+    /**
+     * Fills the task description input
+     * @param taskDescription the description of the task
+     * @returns Promise<void>
+     */
+    async fillTaskDescription (taskDescription: string): Promise<void> {
+        await this.wdioFactoryUtils.setValue(this.locators.taskDescriptionInput, taskDescription);
+    }
+
+    /**
+     * Clicks on the add task button
+     * @returns Promise<void>
+     */
+    async clickAddTaskButton (): Promise<void>  {
+        await this.wdioFactoryUtils.click(this.locators.addTaskButton);
+    }
+
+} export default AddTaskPopUp;
+
+
