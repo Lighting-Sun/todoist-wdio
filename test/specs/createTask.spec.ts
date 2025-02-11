6b5709c565f50518b782475871ed1cf2af641749
HarveyC <harveydavid14@gmail.com>
2025-02-11 11:37:51 -0500

    Add test for creating multiple tasks and validate task names and descriptions

--- test/specs/createTask.spec.ts
+++ test/specs/createTask.spec.ts
@@ -8,6 +8,21 @@ const userPassword = process.env.USERPASSWORD!;
 
 describe('Task creation scenarios', () => {
 
+    it.only('Create 10 tasks', async () => {
+        const taskNames = await UtilsMethods.getArrayOfRandomStrings(10);
+        const taskDescriptions = await UtilsMethods.getArrayOfRandomStrings(10);
+        const expectedTaskNames = await UtilsMethods.arrayToString(taskNames);
+        const expectedTaskDescriptions = await UtilsMethods.arrayToString(taskDescriptions)
+        await loginPage.openPage();
+        await loginPage.loginToPage(userEmail, userPassword);
+        await todayPage.addTask.createMultipleTasksByTaskNameAndDescription(taskNames, taskDescriptions);
+        const atcualTaskNames =  await todayPage.getTaskNames();
+        const actualTaskDescriptions = await todayPage.getTaskDescriptions();
+        await expect(await UtilsMethods.arrayToString(atcualTaskNames)).toContain(expectedTaskNames);
+        await expect(await UtilsMethods.arrayToString(actualTaskDescriptions)).toContain(expectedTaskDescriptions);
+        await todayPage.deleteAllTasksByTasksName(taskNames);
+    });
+
     it('Create a single task @smoke', async () => {
         const taskName = casual.title;
         const taskDescription = casual.short_description;
@@ -24,25 +39,6 @@ describe('Task creation scenarios', () => {
         await expect(await todayPage.getTaskNameByName(taskName)).toEqual(taskName);
         await expect(await todayPage.getTaskDescriptionByName(taskName)).toEqual(taskDescription);
         await todayPage.deleteAllTasksByTasksName([taskName]);
-    });
-
-    it.only('Create 10 tasks', async () => {
-        const taskNames = await UtilsMethods.getArrayOfRandomStrings(10);
-        const taskDescriptions = await UtilsMethods.getArrayOfRandomStrings(10);
-
-        const expectedTaskNames = await UtilsMethods.arrayToString(taskNames);
-        const expectedTaskDescriptions = await UtilsMethods.arrayToString(taskDescriptions)
-
-        await loginPage.openPage();
-        await loginPage.loginToPage(userEmail, userPassword);
-        await todayPage.addTask.createMultipleTasksByTaskNameAndDescription(taskNames, taskDescriptions);
-        const atcualTaskNames =  await todayPage.getTaskNames();
-        const actualTaskDescriptions = await todayPage.getTaskDescriptions();
-        console.log(atcualTaskNames);
-        console.log(actualTaskDescriptions);
-        await expect(await UtilsMethods.arrayToString(atcualTaskNames)).toContain(expectedTaskNames);
-        await expect(await UtilsMethods.arrayToString(actualTaskDescriptions)).toContain(expectedTaskDescriptions);
-        await todayPage.deleteAllTasksByTasksName(taskNames);
     });
 
 });
